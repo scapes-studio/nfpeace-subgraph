6d3fe85e935af96c7408fbcd75f089fe7db3c347
jalil <jalil@1001.digital>
2022-03-01 21:11:03 +0100

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,67 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  NFPeace,
+  AuctionExtended,
+  AuctionInitialised,
+  AuctionSettled,
+  Bid
+} from "../generated/NFPeace/NFPeace"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleAuctionExtended(event: AuctionExtended): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.auctionId = event.params.auctionId
+  entity.endTimestamp = event.params.endTimestamp
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.BIDDING_GRACE_PERIOD(...)
+  // - contract.BID_PERCENTAGE_INCREASE(...)
+  // - contract.CHARITY_ADDRESS(...)
+  // - contract.DEFAULT_STARTING_PRICE(...)
+  // - contract.currentBidPrice(...)
+  // - contract.getAuction(...)
+  // - contract.nextAuctionId(...)
+  // - contract.onERC1155BatchReceived(...)
+  // - contract.onERC1155Received(...)
+  // - contract.onERC721Received(...)
+  // - contract.supportsInterface(...)
+}
+
+export function handleAuctionInitialised(event: AuctionInitialised): void {}
+
+export function handleAuctionSettled(event: AuctionSettled): void {}
+
+export function handleBid(event: Bid): void {}
