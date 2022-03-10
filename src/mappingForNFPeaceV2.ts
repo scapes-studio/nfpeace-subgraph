6a104205926223f786aea3219ff46f8b139cd9b8
jalil <jalil@1001.digital>
2022-03-11 00:26:25 +0100

    Add V2

--- src/mappingForNFPeaceV2.ts
+++ src/mappingForNFPeaceV2.ts
@@ -1,3 +1,4 @@
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import {
   NFPeaceV2,
   AuctionExtended as AuctionExtendedEvent,
@@ -9,11 +10,14 @@ import { Auction, Bid, User } from "../generated/schema"
 
 export function handleAuctionInitialised(event: AuctionInitialisedEvent): void {
   let contract = NFPeaceV2.bind(event.address)
+
   let auctionObj = contract.getAuction(event.params.auctionId);
-  let user = new User(auctionObj.value2.toHex())
+  const donor = auctionObj.value2.toHex()
+  let user = new User(donor)
   user.save()
+
   let auction = new Auction(event.params.auctionId.toString());
-  auction.donor = auctionObj.value2.toHex();
+  auction.donor = donor;
   auction.startingPrice = auctionObj.value4;
   auction.endTimestamp = auctionObj.value5;
   auction.tokenContract = auctionObj.value0;
@@ -21,27 +25,46 @@ export function handleAuctionInitialised(event: AuctionInitialisedEvent): void {
   auction.tokenStandard = auctionObj.value6;
   auction.tokenCount = auctionObj.value7;
   auction.settled = auctionObj.value8;
+  auction.createdAt = event.block.timestamp;
+  auction.createdTransaction = event.transaction.hash.toHex();
   auction.save();
+
+  log.info(`NEW AUCTION #{} FROM {}`, [auction.id, donor])
 }
 
 export function handleAuctionExtended(event: AuctionExtendedEvent): void {
-  let auction = new Auction(event.params.auctionId.toString());
-  auction.endTimestamp = event.params.endTimestamp;
-  auction.save();
+  const id = event.params.auctionId.toString()
+  let auction = Auction.load(id)
+  if (auction == null) {
+    auction = new Auction(id)
+  }
+
+  auction.endTimestamp = event.params.endTimestamp
+  auction.save()
 }
 
 export function handleAuctionSettled(event: AuctionSettledEvent): void {
-  let auction = new Auction(event.params.auctionId.toString());
-  auction.settled = true;
-  auction.save();
+  const id = event.params.auctionId.toString()
+  let auction = Auction.load(id)
+  if (auction == null) {
+    auction = new Auction(id)
+  }
+
+  auction.settledAt = event.block.timestamp
+  auction.settledTransaction = event.transaction.hash.toHex()
+  auction.settled = true
+  auction.save()
 }
 
 export function handleBid(event: BidEvent): void {
-  let bid = new Bid(event.transaction.hash.toHex());
   let user = new User(event.params.from.toHex())
   user.save()
+
+  let bid = new Bid(event.transaction.hash.toHex());
   bid.from = event.params.from.toHex();
   bid.value = event.params.bid;
   bid.auction = event.params.auctionId.toString();
+  bid.createdAt = event.block.timestamp;
+  bid.createdTransaction = event.transaction.hash.toHex();
   bid.save()
 }
